f8e48058ec80d295112a17ae2d1f8feca6548d38
Volodymyr Gamula <v.gamula@gmail.com>
2017-03-08 15:12:21 +0200

    Intial commit

---
+++ content.js
@@ -0,0 +1,23 @@
+/* globls angular */
+
+(function() {
+    setTimeout(function() {
+        var scope = angular.element(document.querySelector('.content.main-page')).scope();
+        var broadcast = scope.broadcastVm;
+        var currentTrack = null;
+        var streamPlayingData = null;
+
+        function checkPlaying() {
+            streamPlayingData = broadcast.isStreamPlaying();
+            if (streamPlayingData.radio) {
+                currentTrack = broadcast.currentRadioData();
+            } else if (streamPlayingData.music) {
+                currentTrack = broadcast.currentMusicData();
+            } else {
+                return;
+            }
+            document.title = currentTrack.artist + ' ' + currentTrack.track;
+        }
+        setInterval(checkPlaying, 1000);
+    }, 3000);
+})();
